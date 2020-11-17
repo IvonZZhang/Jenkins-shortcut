74bb4a7db17ef18231ba32b533fe55c85b043805
Ivonzhang <30924101+IvonZZhang@users.noreply.github.com>
2020-11-17 12:58:22 +0100

    Update Jenkins_config.bat

--- Jenkins_config.bat
+++ Jenkins_config.bat
@@ -273,8 +273,8 @@ REM ==============================================
 echo Set up squish
 REM ==============================================
 :: Setup environment for squish
-IF "%JENKINS_BUILD_TYPE%"=="release" IF "%JENKINS_BUILD_MODE%"=="32bit" call app load squish/6.5.2_32bit
-IF "%JENKINS_BUILD_TYPE%"=="release" IF "%JENKINS_BUILD_MODE%"=="64bit" call app load squish/6.5.2
+IF "%JENKINS_BUILD_TYPE%"=="release" IF "%JENKINS_BUILD_MODE%"=="32bit" call app load squish/6.5.2_qt512_32bit
+IF "%JENKINS_BUILD_TYPE%"=="release" IF "%JENKINS_BUILD_MODE%"=="64bit" call app load squish/6.5.2_qt512
 REM Debug versions will be loaded in setup_squish, so no need of manual loading.
 call app load "%JENKINS_BUILD_MODE%" %ULOGRROOT%\setup_squish
 @echo on
