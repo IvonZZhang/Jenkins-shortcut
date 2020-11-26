4297a598986760258184d3bee95fce8c15e85c93
Ivonzhang <30924101+IvonZZhang@users.noreply.github.com>
2020-11-26 10:50:57 +0100

    Update tasktest.bat

--- tasktest.bat
+++ tasktest.bat
@@ -1,2 +1,10 @@
-tasklist /FI "USERNAME eq jenkins" | sort
+suite=abc ^
+  def ^
+  ghi ^
+  jkl
+for %%s in ("%suite%") do (
+  echo %%s
+  tasklist /FI "USERNAME eq jenkins" | sort
+)
+REM tasklist /FI "USERNAME eq jenkins" | sort
 echo finished
