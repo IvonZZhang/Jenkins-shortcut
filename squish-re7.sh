3a6d1a541b64b740748dace4ca4ee725797dadea
Ivonzhang <30924101+IvonZZhang@users.noreply.github.com>
2021-05-05 16:52:07 +0200

    Fix $suite_name. Try single suite with 5.12.8-ltcg

--- squish-re7.sh
+++ squish-re7.sh
@@ -11,7 +11,8 @@ module purge
 set +x
 module load $PWD/cbs/setup_HOST
 module load dot
-app switch qt qt/5.12.8_64bit
+# app switch qt qt/5.12.8_64bit
+app switch qt qt/5.12.8_gcc-7.3.0-ltcg_64bit
 app switch gcc gcc/4.9.1_64bit
 app list
 set -x
@@ -216,42 +217,43 @@ app list
 
 cd $WORKSPACE
 
-# squishrunner --testsuite $WORKSPACE/uLogR/src/plugins/memory_viewer/tests/suite_BDD_MemoryViewer --local --tags '~@target' --tags '~@T_ULOGR-1346' --tags '~@workinprogress' --tags '~@replay' --tags '~@deprecated' --reportgen xml2.2,$WORKSPACE/squishrunner_report_xml/squishrunner_report.xml --reportgen html,$WORKSPACE/squishrunner_report_html/ --reportgen stdout
-for suite_dir in $(ls -d $ULOGRROOT/src/core/tests/suite_* $ULOGRROOT/src/plugins/*/tests/suite_* $ULOGRROOT/src/api/tests/suite_*)
-do
-    # Deprecated test suites
-    if [[ $suite_dir == */suite_BDD_FlashFSViewer ]] || [[ $suite_dir == */suite_BDD_BandWidth ]] || [[ $suite_dir == */suite_BDD_MessageConsole ]]
-    then
-        continue
-    fi
-    if contains "$suite_dir" suite_BDD_Api
-    then
-        timeout="--timeout 3000"
-    else
-        timeout=""
-    fi   
+squishrunner --testsuite $WORKSPACE/uLogR/src/plugins/memory_viewer/tests/suite_BDD_MemoryViewer --local --tags '~@target' --tags '~@T_ULOGR-1346' --tags '~@workinprogress' --tags '~@replay' --tags '~@deprecated' --reportgen xml2.2,$WORKSPACE/squish_report_xml/squish_report.xml --reportgen html,$WORKSPACE/squish_report_html/ --reportgen stdout | tee -a $ULOGRBUILD/squish.out 2>&1
+# for suite_dir in $(ls -d $ULOGRROOT/src/core/tests/suite_* $ULOGRROOT/src/plugins/*/tests/suite_* $ULOGRROOT/src/api/tests/suite_*)
+# do
+#     # Deprecated test suites
+#     if [[ $suite_dir == */suite_BDD_FlashFSViewer ]] || [[ $suite_dir == */suite_BDD_BandWidth ]] || [[ $suite_dir == */suite_BDD_MessageConsole ]]
+#     then
+#         continue
+#     fi
+#     if contains "$suite_dir" suite_BDD_Api
+#     then
+#         timeout="--timeout 3000"
+#     else
+#         timeout=""
+#     fi   
 
-    set -x
-    remove_ulogr_config
+#     set -x
+#     remove_ulogr_config
     
-    suite_name = echo $suite_dir | sed -E "s/^.*\/(\w+)/\1/"
-    set +x
-    echo
-    echo "------------------ START: $suite_name --------------------------
-    echo
-    set -x
-    squishrunner --testsuite $suite_dir --local $timeout \
-        $SQUISHRUNNER_TAGS \
-        --reportgen xml2.2,$WORKSPACE/squish_report_xml/squish_report_${suite_name}.xml \
-        --reportgen html,$WORKSPACE/squish_report_html/ \
-        --reportgen stdout \
-        | tee $ULOGRBUILD/squish.out 2>&1
-    set +x
-    echo
-    echo "------------------ FINISHED: $suite_name --------------------------
-    echo
-    set -x
-done
+#     suite_name=$(echo $suite_dir | sed -E "s/^.*\/(\w+)/\1/")
+#     set +x
+#     echo
+#     echo "------------------ START: $suite_name --------------------------
+#     echo
+#     set -x
+#     squishrunner --testsuite $suite_dir --local $timeout \
+#         $SQUISHRUNNER_TAGS \
+#         --reportgen xml2.2,$WORKSPACE/squish_report_xml/squish_report_${suite_name}.xml \
+#         --reportgen html,$WORKSPACE/squish_report_html/ \
+#         --reportgen stdout \
+#         | tee -a $ULOGRBUILD/squish.out 2>&1
+#     set +x
+#     echo
+#     echo "------------------ FINISHED: $suite_name --------------------------
+#     echo
+#     set -x
+# done
+
 echo "=============================================="
 echo "Clean up Squish and VNC"
 echo "=============================================="
