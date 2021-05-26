432ec43dd8048538a4ec6781c38c35f083388429
Ivonzhang <30924101+IvonZZhang@users.noreply.github.com>
2021-05-26 17:34:12 +0200

    Added --retry 2

--- squish-re7.sh
+++ squish-re7.sh
@@ -232,7 +232,7 @@ do
     echo "------------------ START: $suite_name --------------------------"
     echo
     set -x
-    squishrunner --testsuite $suite_dir --local $timeout $SQUISHRUNNER_TAGS --reportgen xml2.2,$WORKSPACE/squish_report_xml/squish_report_${suite_name}.xml --reportgen html,$WORKSPACE/squish_report_html/ --reportgen stdout > >(tee -a $ULOGRBUILD/squish.out) 2> >(tee -a $ULOGRBUILD/squish.out >&2)
+    squishrunner --testsuite $suite_dir --local --retry 2 $timeout $SQUISHRUNNER_TAGS --reportgen xml2.2,$WORKSPACE/squish_report_xml/squish_report_${suite_name}.xml --reportgen html,$WORKSPACE/squish_report_html/ --reportgen stdout > >(tee -a $ULOGRBUILD/squish.out) 2> >(tee -a $ULOGRBUILD/squish.out >&2)
     set +x
     echo
     echo "------------------ FINISHED: $suite_name ------------------------"
