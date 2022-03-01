833ea19ee149d02779253d259b83f6025d36ca7b
Ivonzhang <30924101+IvonZZhang@users.noreply.github.com>
2022-03-01 12:14:01 +0100

    Update Jenkins_config.sh

--- Jenkins_config.sh
+++ Jenkins_config.sh
@@ -466,7 +466,7 @@ cd $WORKSPACE
 export evaluate_squish_report=$WORKSPACE/uLogR/src/tests/squish/scripts/evaluate_squish_report.py
 > build.status
 if [[ -f "$SQUISH_REPORT_DIR/squishrunner_report_xml/*.xml" ]]; then
-  python $evaluate_squish_report --tag @workinprogress $SQUISH_REPORT_DIR/squishrunner_report_xml/*.xml --result $SQUISH_REPORT_DIR/squishrunner_report_html/data/results-v1.js
+  python $evaluate_squish_report --tag @workinprogress $SQUISH_REPORT_DIR/squish_report_xml/*.xml --result $SQUISH_REPORT_DIR/squish_report_html/data/results-v1.js
 fi
 
 BUILD_STATUS=""
